fe45d31a1e79a8b4d97624394af1b0356ea7f012
Shir Bar <63155990+shir3bar@users.noreply.github.com>
2023-07-19 20:19:55 +0300

    added colab utils

---
+++ colab_env.sh
@@ -0,0 +1,3 @@
+pip install -U git+https://github.com/facebookresearch/fvcore.git git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI
+pip install -U git+https://github.com/facebookresearch/detectron2
+pip install -U git+https://github.com/facebookresearch/slowfast
